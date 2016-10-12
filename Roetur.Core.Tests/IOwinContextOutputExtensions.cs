e7b9b08b58a36a0fa71350c21490f4dd9292f253
TheAllSeeingPie <joeswan@me.com>
2016-10-12 14:26:55 +0100

    Moved tests to use Moq instead using the quickest route possible.
    Also sneaked in an update to framework and some tidying up of Nuget usage

--- Roetur.Core.Tests/IOwinContextOutputExtensions.cs
+++ Roetur.Core.Tests/IOwinContextOutputExtensions.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.Owin.Fakes;
+using Microsoft.Owin;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Roetur.Core.Tests
 {
@@ -11,15 +12,12 @@ namespace Roetur.Core.Tests
         [TestMethod]
         public async Task Ok_should_return_200_Ok()
         {
-            var stubIOwinRequest = new StubIOwinRequest
-            {
-                UriGet = () => new Uri("http://localhost/"),
-                MethodGet = () => "GET"
-            };
-            var context = new StubIOwinContext
-            {
-                RequestGet = () => stubIOwinRequest
-            };
+            var request = new Mock<IOwinRequest>();
+            request.Setup(r => r.Uri).Returns(new Uri("http://localhost/"));
+            request.Setup(r => r.Method).Returns("GET");
+
+            var context = new Mock<IOwinContext>();
+            context.Setup(c => c.Request).Returns(request.Object);
         }
     }
 }
